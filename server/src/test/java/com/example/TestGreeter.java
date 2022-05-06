21587ec9a55753f031db5ac11c07c44c7af239ad
Tarek Nasr <tarek.a.nasr@gmail.com>
2022-05-07 01:15:46 +0200

    linter

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -1,11 +1,33 @@
-package com.vogella.junit.first;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
+package com.example;
 
-class TestGreeter {
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class TestGreeter {
+
+  private Greeter greeter;
+
+  @Before
+  public void setup() {
+    greeter = new Greeter();
+  }
 
   @Test
-  void demoTestMethod() {
-    assertTrue(true);
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
+
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 }
